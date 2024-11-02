841f27d49aa7e49936cff89745791d9b133d84d3
JahanZaibOctaloop <jahanzaibasif898@gmail.com>
2024-11-02 20:25:39 +0500

    update the code

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
 const path = require('path');
+require('dotenv').config();
 const port = process.env.PORT || 3000;
 
 
